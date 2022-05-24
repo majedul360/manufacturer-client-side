bc2bd7b3da083d8391efdd5e6bbfec899389b9fc
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-05-24 09:29:24 +0600

    orders and cancel orders api done

--- index.js
+++ index.js
@@ -62,6 +62,22 @@ const run = async () => {
       const result = await purchaseCollection.insertOne(purchaseProduct);
       res.send(result);
     });
+
+    // Load all purchases products as orders
+    app.get("/orders/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const result = await purchaseCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // delete purchase or ordered product
+    app.delete("/cancelOrder/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await purchaseCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 };
